66e6b877e5fc18cab8804823e917e34fdd8e07dc
SamFernFer <>
2024-09-15 06:38:45 -0300

    Most probably everything works now. I tested them.

--- CompareHashTables.py
+++ CompareHashTables.py
@@ -1,6 +1,6 @@
 import sys, os, json
 
-if len(sys.argv < 3):
+if len(sys.argv) < 3:
     print(
         "Compares two JSON hash tables. The first file is considered "
         + "the reference one when listing the differences."
@@ -15,18 +15,34 @@ with open(_firstPath) as _firstFile:
     with open(_secondPath) as _secondFile:
         _firstTable: dict = json.load(_firstFile)
         _secondTable: dict = json.load(_secondFile)
+        _hasMissing: bool = False
+        _hasDifferent: bool = False
+        _hasExtra: bool = False
         
         for k,v in _firstTable.items():
             _other = _secondTable.get(k, None)
             # The second table does not contain the file.
             if _other is None:
-                print("Missing: {0}".format(json.dumps(k)))
+                print("[Missing]: {0}".format(json.dumps(k)))
+                _hasMissing = True
             # The second table's file has a different hash.
             elif v != _other:
-                print("Different: {0}".format(json.dumps(k)))
+                print("[Different]: {0}".format(json.dumps(k)))
+                _hasDifferent = True
             # Removes the entry from the second table for later checking for extra files.
             _secondTable.pop(k)
 
         # List the extra files from the second table.
-        for k,v in _secondTable:
-                print("Extra: {0}".format(json.dumps(k)))
+        for k,v in _secondTable.items():
+            print("[Extra]: {0}".format(json.dumps(k)))
+            _hasExtra = True
+
+        if not _hasMissing:
+            print("No missing files.")
+        if not _hasDifferent:
+            print("No different files.")
+        if not _hasExtra:
+            print("No extra files.")
+        if not _hasMissing and not _hasDifferent and not _hasExtra:
+            print("Both trees are equal.")
+        
